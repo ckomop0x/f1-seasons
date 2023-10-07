75c56d73756d2a0132067222b72a5b7eee534b9a
Pavel Klochkov <paul.klochkov@gmail.com>
2023-10-07 16:36:17 +0200

    Initial Playwright setup

--- test/unit/components/Seasons/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as renderer from 'react-test-renderer';
-import Seasons from "../../../../src/components/Seasons";
-
-it("renders the heading", () => {
-    const wrapper = renderer.create(<Seasons/>).toJSON();
-
-    expect(wrapper).toMatchSnapshot()
-});
