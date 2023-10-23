ddcd7e372be9fada65574303f87fa0e4c417aa20
Pavel Klochkov <paul.klochkov@gmail.com>
2023-10-24 01:01:58 +0200

    Remove some rubbish from racePage test file

--- e2e/racePage.test.ts
+++ e2e/racePage.test.ts
@@ -15,27 +15,3 @@ test('Season 2020 race have proper h2', async ({ page }) => {
   // await expect(h1Element).toExist();
   await expect(textValue).toBe('Red Bull Ring');
 });
-
-// test('Main page has correct meta tags', async ({ page }) => {
-//   await page.goto('/');
-//   await expect(page.locator("//link[@rel='canonical']")).toHaveAttribute(
-//     'href',
-//     'https://ondrabus.com/',
-//   );
-//   await expect(page.locator("//meta[@property='og:url']")).toHaveAttribute(
-//     'content',
-//     'https://ondrabus.com/',
-//   );
-// });
-
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-//
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-//
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole('heading', { name: 'Installation' }),
-//   ).toBeVisible();
-// });
